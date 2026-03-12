7702d043cc0fdfa73110ece01980313096c8d811
CoderK-star <a250842t@reikaku.jp>
2026-03-13 06:14:26 +0900

    deploy configuration

---
+++ Map/js/config.js
@@ -0,0 +1,9 @@
+// API Keys - DO NOT commit this file to version control!
+// This file is listed in .gitignore.
+// Copy config.example.js to config.js and fill in your keys.
+
+window.API_KEYS = {
+    MAPTILER_KEY: 'z2iOmrIdt1L9Yw9QsXS3',
+    GOOGLE_SHEETS_KEY: 'AIzaSyAUi4KazffmDZV_dQUnMUKA1jJt4i0mqlU',
+    GTAG_ID: 'G-Y4J0KEG3PF'
+};
